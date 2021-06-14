c4dbad13fe807ecb2857136797a04480851dad5c
Michael Nnaji <mnnaji3@uwo.ca>
2021-06-14 00:29:59 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,30 +1,17 @@
-class StudentsController < ApplicationController
-  def new
-    @student = Student.new
-  end
-
-  def create
-    @student = Student.new(student_params)
-    if @student.save
-      redirect_to @student
-    else
-      render 'new'
-    end
-  end
-
-  def edit
-    @student = Student.find(params[:id])
-  end
+class Students < ApplicationController
 
   def show
     @student = Student.find(params[:id])
+      @students = @classroom.students
   end
 
   def index
-    @students = Student.all
+    @students = Student.search(params[:query])
+    render 'index'
   end
 
   def student_params
     params.require(:student).permit(:name, :birthday, :hometown)
   end
+
 end
